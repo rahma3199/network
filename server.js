447ca49f8e887e3d6365b4127cb097d1a01a7d95
rahma3199 <rahmaaloui3199@gmail.com>
2023-10-23 11:32:06 -0500

    Your commit message

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const multer = require('multer');
+
 const app = express();
 const port = 3000;
 
+// Set storage engine
 const storage = multer.diskStorage({
-    destination: './uploads', // Specify the destination folder for uploaded images.
-    filename: function (req, file, callback) {
-        // Generate a unique filename for the uploaded image.
-        callback(null, Date.now() + '-' + file.originalname);
-    },
+  destination: './uploads/',
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.').pop());
+  }
 });
 
+// Initialize upload
 const upload = multer({ storage: storage });
 
-// Serve your HTML page.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname)); // Serve your HTML file
 
-// Handle POST requests for image uploads.
-app.post('/upload', upload.single('image'), (req, res) => {
-    // Check if the file was successfully uploaded.
-    if (!req.file) {
-        return res.json({ success: false });
-    }
+app.post('/upload', upload.single('sampleFile'), (req, res) => {
+  if (!req.file) {
+    return res.json({ success: false, message: 'No file uploaded.' });
+  }
+  
+  // Normally, here is where you'd do processing on the uploaded file if necessary.
+  // But for simplicity, we'll just send a success response.
 
-    // If the file was uploaded successfully, send a success response.
-    res.json({ success: true });
+  res.json({ success: true, message: 'File uploaded successfully.' });
 });
 
-// Start the server.
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server started on http://localhost:${port}`);
 });
